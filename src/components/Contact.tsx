a0317ec1294de7fcf9afcd8ba93cf584c6cdae51
mad4thrash <fabio.plumeri@oisluxurygroup.com>
2025-11-02 18:20:05 +0100

    implemented footer (contacts)

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,98 @@
+import { useGSAP } from "@gsap/react";
+import { openingHours, socials } from "../constants";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+
 const Contact = () => {
-	return <div>Contact</div>;
+	useGSAP(() => {
+		const titleSplit = SplitText.create("#contact h2", {
+			type: "words",
+		});
+
+		const timeLine = gsap.timeline({
+			scrollTrigger: {
+				trigger: "#contact",
+				start: "top center",
+			},
+			ease: "power1.inOut",
+		});
+
+		timeLine
+			.from(titleSplit.words, {
+				opacity: 0,
+				yPercent: 100,
+				stagger: 0.02,
+			})
+			.from("#contact h3", {
+				opacity: 0,
+				yPercent: 100,
+				stagger: 0.02,
+			})
+			.to("#f-right-leaf", {
+				y: "-50",
+				duration: 1,
+				ease: "power1.inOut",
+			})
+			.to(
+				"#f-left-leaf",
+				{
+					y: "-50",
+					duration: 1,
+					ease: "power1.inOut",
+				},
+				"<"
+			);
+	});
+	return (
+		<footer id="contact">
+			<img
+				src="/images/footer-right-leaf.png"
+				alt="leaf-right"
+				id="f-right-leaf"
+			/>
+			<img
+				src="/images/footer-left-leaf.png"
+				alt="leaf-left"
+				id="f-left-leaf"
+			/>
+			<div className="content">
+				<h2>Where to Find Us</h2>
+				<div>
+					<h3>Visit Our Bar</h3>
+					<p>456, Raq Blvd. #404, Los Angeles, CA 90210</p>
+				</div>
+				<div>
+					<h3>Contact Us</h3>
+					<p>(555) 987-6543</p>
+					<p>hello@madforcocktails.com</p>
+				</div>
+				<div>
+					<h3>Open Every Day</h3>
+					{openingHours.map((el) => (
+						<p key={el.day}>
+							{el.day} : {el.time}
+						</p>
+					))}
+				</div>
+				<div>
+					<h3>Socials</h3>
+					<div className="flex-center gap-5">
+						{socials.map((social) => (
+							<a
+								href={social.url}
+								target="_blank"
+								key={social.name}
+								rel="noopener noreferrer"
+								aria-label={social.name}
+							>
+								<img src={social.icon} alt={social.name} />
+							</a>
+						))}
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Contact;
